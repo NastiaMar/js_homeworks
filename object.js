b9f61ba1812352dee53606dcdd2914a87a24efd1
NastiaMar <nastiamarchenko96@gmail.com>
2025-03-28 15:48:08 +0200

    Update code

--- object.js
+++ object.js
@@ -1,25 +1,29 @@
 var services = {
-    "стрижка": "60 грн",
-    "гоління": "80 грн",
-    "Миття голови": "100 грн"
+    "Стрижка": "60 грн",
+    "Гоління": "80 грн",
+    "Миття голови": "100 грн",
+    "Розбити скло": "200 грн",
+
+    price() {
+        return Object.values(this)
+            .filter(value => typeof value === "string" && value.includes("грн"))
+            .map(value => parseInt(value))
+            .reduce((sum, current) => sum + current, 0) + " грн";
+    },
+
+    minPrice() {
+        return Math.min(...Object.values(this)
+            .filter(value => typeof value === "string" && value.includes("грн"))
+            .map(value => parseInt(value))) + " грн";
+    },
+
+    maxPrice() {
+        return Math.max(...Object.values(this)
+            .filter(value => typeof value === "string" && value.includes("грн"))
+            .map(value => parseInt(value))) + " грн";
+    }
 };
 
-services['Розбити скло'] = "200 грн";
-
-function price() {
-    return Object.values(services)
-        .map(value => parseInt(value)) 
-        .reduce((sum, current) => sum + current, 0) + " грн";
-}
-
-function minPrice() {
-    return Math.min(...Object.values(services).map(value => parseInt(value))) + " грн";
-}
-
-function maxPrice() {
-    return Math.max(...Object.values(services).map(value => parseInt(value))) + " грн";
-}
-
-console.log("Загальна вартість:", price());
-console.log("Мінімальна ціна:", minPrice());
-console.log("Максимальна ціна:", maxPrice());
+console.log("Загальна вартість:", services.price());
+console.log("Мінімальна ціна:", services.minPrice());
+console.log("Максимальна ціна:", services.maxPrice());
